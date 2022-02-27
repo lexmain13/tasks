3160395c936cc6aa81ef8f4252a76ddec4d59810
Alexis Mainiero <lexmain@udel.edu>
2022-02-27 18:10:51 -0500

    wrote and finished addNewQuestion

--- src/nested.ts
+++ src/nested.ts
@@ -1,5 +1,6 @@
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
+import { makeBlankQuestion } from "./objects";
 
 /**
  * Consumes an array of questions and returns a new array with only the questions
@@ -163,7 +164,9 @@ export function addNewQuestion(
     name: string,
     type: QuestionType
 ): Question[] {
-    return [];
+    const blankQuestion = makeBlankQuestion(id, name, type);
+    const updatedQuestionArray = [...questions, blankQuestion];
+    return updatedQuestionArray;
 }
 
 /***
