26a5fa2e74e20167074eb013adae6ff3046bcb86
Alexis Mainiero <lexmain@udel.edu>
2022-02-28 22:37:40 -0500

    wrote, fixed, and completed sameType function

--- src/nested.ts
+++ src/nested.ts
@@ -159,7 +159,21 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    const shortAnswerEqual = questions.every(
+        (question: Question): boolean =>
+            question.type === "short_answer_question"
+    );
+    const multipleAnswerEqual = questions.every(
+        (question: Question): boolean =>
+            question.type === "multiple_choice_question"
+    );
+    if (shortAnswerEqual) {
+        return true;
+    } else if (multipleAnswerEqual) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /***
