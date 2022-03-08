78806beb28ab9b123fb53e61f892979bb6ef118c
Alexis Mainiero <lexmain@udel.edu>
2022-03-08 00:59:04 -0500

    wrote and completed ChangeType and RevealAnswer

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -1,7 +1,23 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+import { scryRenderedComponentsWithType } from "react-dom/test-utils";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [question, setQuestion] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    function flipType(): void {
+        if (question === "short_answer_question") {
+            setQuestion("multiple_choice_question");
+        } else {
+            setQuestion("short_answer_question");
+        }
+    }
+    return (
+        <div>
+            <Button onClick={flipType}>Change Type</Button>
+            Question Type: <span>{question}</span>
+        </div>
+    );
 }
