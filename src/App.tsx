b7d790074741bff4d4f30d4e41a2d6b790fd64d5
Alexis Mainiero <lexmain@udel.edu>
2022-03-18 18:39:07 -0400

    put all components behind a button to toggle visibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
@@ -16,8 +16,14 @@ import { EditMode } from "./form-components/EditMode";
 import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
 import { ChangeColor } from "./form-components/ChangeColor";
 import { Quizzer } from "./quizzer/Quizzer";
+import { Button } from "react-bootstrap";
 
 function App(): JSX.Element {
+    const [visible, setVisible] = useState<boolean>(false);
+    function flipVisibility(): void {
+        setVisible(!visible);
+    }
+
     return (
         <div className="App">
             <header className="App-header">
@@ -25,39 +31,44 @@ function App(): JSX.Element {
                 <h1>Alexis Mainiero</h1>
             </header>
             <Quizzer></Quizzer>
-            <hr></hr>
-            <CheckAnswer expectedAnswer="42"></CheckAnswer>
-            <hr></hr>
-            <GiveAttempts></GiveAttempts>
-            <hr></hr>
-            <EditMode></EditMode>
-            <hr></hr>
-            <ChangeColor></ChangeColor>
-            <hr></hr>
-            <MultipleChoiceQuestion
-                options={["a", "b", "c"]}
-                expectedAnswer="b"
-            ></MultipleChoiceQuestion>
-            <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
-            <hr></hr>
-            <ChooseTeam></ChooseTeam>
-            <hr></hr>
-            <ColoredBox></ColoredBox>
-            <hr></hr>
-            <ShoveBox></ShoveBox>
-            <hr></hr>
-            <Counter></Counter>
-            <hr />
-            <RevealAnswer></RevealAnswer>
-            <hr />
-            <StartAttempt></StartAttempt>
-            <hr />
-            <TwoDice></TwoDice>
-            <hr />
-            <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
+            <div>
+                <Button onClick={flipVisibility}>Show Components</Button>
+                {visible && (
+                    <>
+                        <hr></hr>
+                        <CheckAnswer expectedAnswer="42"></CheckAnswer>
+                        <hr></hr>
+                        <GiveAttempts></GiveAttempts>
+                        <hr></hr>
+                        <EditMode></EditMode>
+                        <hr></hr>
+                        <ChangeColor></ChangeColor>
+                        <hr></hr>
+                        <MultipleChoiceQuestion
+                            options={["a", "b", "c"]}
+                            expectedAnswer="b"
+                        ></MultipleChoiceQuestion>
+                        <hr></hr>
+                        <ChooseTeam></ChooseTeam>
+                        <hr></hr>
+                        <ColoredBox></ColoredBox>
+                        <hr></hr>
+                        <ShoveBox></ShoveBox>
+                        <hr></hr>
+                        <Counter></Counter>
+                        <hr />
+                        <RevealAnswer></RevealAnswer>
+                        <hr />
+                        <StartAttempt></StartAttempt>
+                        <hr />
+                        <TwoDice></TwoDice>
+                        <hr />
+                        <ChangeType></ChangeType>
+                        <hr />
+                        <CycleHoliday></CycleHoliday>
+                    </>
+                )}
+            </div>
         </div>
     );
 }
