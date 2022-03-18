e18d6fc3bcf068569a715c057717dd110b33232c
Alexis Mainiero <lexmain@udel.edu>
2022-03-18 00:56:38 -0400

    CheckAnswer is completed and passes tests. EditMode works but is failing tests

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,57 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [name, setName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+    const [isEditing, setIsEditing] = useState<boolean>(true);
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <div>
+                <Form.Switch
+                    type="switch"
+                    id="is-edit-mode"
+                    label="Editing"
+                    checked={isEditing}
+                    onChange={() => setIsEditing(!isEditing)}
+                />
+            </div>
+            <div>
+                {isEditing ? (
+                    "In Edit Mode"
+                ) : isStudent ? (
+                    <div>
+                        <span>{name}</span> is a student
+                    </div>
+                ) : (
+                    <div>
+                        <span>{name}</span> is a not student
+                    </div>
+                )}
+            </div>
+            <div>
+                <Form.Check
+                    type="checkbox"
+                    id="is-student"
+                    label="Student"
+                    checked={isStudent}
+                    onChange={() => setIsStudent(!isStudent)}
+                />
+            </div>
+            <div>
+                <Form.Group>
+                    <Form.Label>Name:</Form.Label>
+                    <Form.Control
+                        type="textbox"
+                        value={name}
+                        onChange={(
+                            event: React.ChangeEvent<HTMLInputElement>
+                        ) => setName(event.target.value)}
+                    />
+                </Form.Group>
+            </div>
         </div>
     );
 }
